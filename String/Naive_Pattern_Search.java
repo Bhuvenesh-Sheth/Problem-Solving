ecf1e9ceca31dd044d77ff3c0da5570ba9210adf
Bhuvenesh-Sheth <shethbhuvenesh@gmail.com>
2023-09-12 11:23:36 +0530

    String Updated

--- String/Naive_Pattern_Search.java
+++ String/Naive_Pattern_Search.java
@@ -27,32 +27,6 @@ Note: N = |S|, M = |P|.
 Constraints:
 1 <= |S|, |P| <= 103
  */
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class gfg {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            String s = "", p = "";
-            s = sc.next();
-            p = sc.next();
-
-            Solution obj = new Solution();
-
-            if (obj.search(p, s) == true)
-                System.out.println("Yes");
-            else
-                System.out.println("No");
-        }
-    }
-}
-// } Driver Code Ends
-
 class Naive_Pattern_Search {
     // Function to check if the given pattern exists in the given string or not.
     static boolean search(String pat, String txt) {
