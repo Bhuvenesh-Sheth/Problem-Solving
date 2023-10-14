8e51741fd2f121f9c100aa096ab0ec7db0703968
Bhuvenesh-Sheth <shethbhuvenesh@gmail.com>
2023-10-14 20:34:46 +0530

    Linked List Updated

---
+++ Linked List/Rotate_a_Linked_List.java
@@ -0,0 +1,62 @@
+/*
+ *Given a singly linked list of size N. The task is to left-shift the linked list by k nodes, where k is a given positive integer smaller than or equal to length of the linked list.
+
+Example 1:
+
+Input:
+N = 5
+value[] = {2, 4, 7, 8, 9}
+k = 3
+Output: 8 9 2 4 7
+Explanation:
+Rotate 1: 4 -> 7 -> 8 -> 9 -> 2
+Rotate 2: 7 -> 8 -> 9 -> 2 -> 4
+Rotate 3: 8 -> 9 -> 2 -> 4 -> 7
+Example 2:
+
+Input:
+N = 8
+value[] = {1, 2, 3, 4, 5, 6, 7, 8}
+k = 4
+Output: 5 6 7 8 1 2 3 4
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function rotate() which takes a head reference as the first argument and k as the second argument, and returns the head of the rotated linked list.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 103
+1 <= k <= N 
+ */
+public class Rotate_a_Linked_List {
+    public Node rotate(Node head, int k) {
+        // add code here
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node temp = head;
+        Node tail = null;
+        Node mid = null;
+
+        int count = 0;
+
+        while (temp != null) {
+            if (count == k - 1) {
+                mid = temp;
+            }
+
+            tail = temp;
+            temp = temp.next;
+            count++;
+        }
+
+        tail.next = head;
+        head = mid.next;
+        mid.next = null;
+
+        return head;
+
+    }
+}
