a3f6c3b8b8afd6f7bb97e50ee515f61ebbd74d39
Bhuvenesh-Sheth <shethbhuvenesh@gmail.com>
2023-11-04 20:34:53 +0530

    Deque Updated

---
+++ Deque/Dequeue_Traversal.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+
+class Dequeue_Traversal {
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        // Your code here
+        for (Integer x : deq) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
