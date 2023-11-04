57bb57173033e00b232aa6521d5dec85a62c4b54
Bhuvenesh-Sheth <shethbhuvenesh@gmail.com>
2023-11-04 20:34:58 +0530

    Deque Updated

---
+++ Deque/Insertion_in_deque.java
@@ -0,0 +1,13 @@
+import java.util.ArrayDeque;
+
+public class Insertion_in_deque {
+    ArrayDeque<Integer> ad = new ArrayDeque<>();
+
+    for(
+    int i = 0;i<n;i++)
+    {
+        ad.offerLast(arr[i]);
+    }
+
+    return ad;
+}
